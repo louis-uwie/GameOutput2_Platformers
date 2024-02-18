d7eab94eee7f2a739c1844de0d7710d4aa9cce57
uwie <louis.binwag@student.ateneo.edu>
2024-02-18 22:01:43 +0800

    Squashed commit of the following:
    commit b3e6b4d07e5e38b6de43419eca99a795cdba0e22
    Author: uwie <louis.binwag@student.ateneo.edu>
    Date:   Sun Feb 18 22:01:22 2024 +0800
    
        Fixed One-Way Collisions. Moved Player
    
    commit 7ad191ba32fe6adef922b6c5b39092021433b1ce
    Merge: 0271567 11084e3
    Author: uwie <louis.binwag@student.ateneo.edu>
    Date:   Sun Feb 18 21:53:23 2024 +0800
    
        Merge branch 'playerCollision' into developmentBranch
    
    commit 02715674dc0d041bae4193da72d256b29a647a84
    Merge: 88d7293 fbe4ac6
    Author: uwie <louis.binwag@student.ateneo.edu>
    Date:   Sun Feb 18 21:53:08 2024 +0800
    
        Merge branch 'playerMovementFinal' into developmentBranch
    
    commit 11084e37935fc7f71c65f7b421f479777a88628d
    Author: uwie <louis.binwag@student.ateneo.edu>
    Date:   Sun Feb 18 21:40:14 2024 +0800
    
        Added Collision Areas, Misc Fixes
    
        *World Border
        *New Platforms
        *Misc Platforms
        *Adjusted JumpHeight and PlayerSpeed
    
    commit fbe4ac6f5ad156fc549275153402bae0316153e3
    Author: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    Date:   Sun Feb 18 02:02:47 2024 +0800
    
        Add animations to movement & duck through platforms
    
    commit 38560ac68413286ec6b2eeb3dc17b42227fc7c4a
    Author: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    Date:   Sun Feb 18 01:15:08 2024 +0800
    
        Add player movement
    
    commit 13375d6cc459155d956e3164b6fbc280e7be5e38
    Author: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    Date:   Sun Feb 18 00:54:42 2024 +0800
    
        add player to game map
    
    commit 503ad3c047d07a92ce4c8d3a595960879e4b0cc9
    Author: Angelico Teaño <angelico.teano@obf.ateneo.edu>
    Date:   Sun Feb 18 00:48:57 2024 +0800
    
        Add animations and collisionShape to player
    
    commit 88d7293ccffedd8e30de899d347c59a5c1c20d0e
    Merge: 855f1f1 a7bdbac
    Author: uwie <157711065+louis-uwie@users.noreply.github.com>
    Date:   Thu Feb 15 17:28:42 2024 +0800
    
        Merge pull request #1 from louis-uwie/gameMapBranch
    
        Added map
    
    commit a7bdbac64208ea9535b7b4899b2b92c77f640a96
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Thu Feb 15 16:12:35 2024 +0800
    
        Minor changes and fix to Map Design
    
        - removed background tiles beyond the world border
        - added more trees and misc. objects
    
    commit a02f900db14e318b1ab49b2a2d14e149d0273359
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Thu Feb 15 01:42:52 2024 +0800
    
        Updated project settings and minor changes
    
        - changed texture to 'nearest'
        - renamed layers for consistency
    
    commit 19d4c9ff9a27a89a13f25f08de70a88663152bb6
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Thu Feb 15 01:31:09 2024 +0800
    
        Updated map design
    
        - minor additions to design and fixes
    
    commit 82f1fd4a42a9a6324902ccb4dccaeaf23f4f1f08
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Thu Feb 15 01:16:24 2024 +0800
    
        Added Draft Map Design
    
        - added new layers in game map
        - will still update and polish
    
    commit 795ce25035fc1646b348de7d6338656b95df859d
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Wed Feb 14 21:33:12 2024 +0800
    
        Deleted unnecessary sprites
    
        - deleted sprites that were not considered map tiles
    
    commit f974de399f2f83a82d97c5d3e16e2857a027c33f
    Author: Paolo <118610859+Pencisas@users.noreply.github.com>
    Date:   Wed Feb 14 21:31:52 2024 +0800
    
        Added sprites folder and Map sprite inside
    
        - created new sprites folder in the project files inside assets folder
        - placed inside the map sprites (High Forest)
    
    commit a90e859c705f79f12e3a30737641691983b14489
    Author: uwie <louis.binwag@student.ateneo.edu>
    Date:   Mon Feb 12 18:20:01 2024 +0800
    
        Update gameMap.gd

--- assets/scripts/playerBody.gd
+++ assets/scripts/playerBody.gd
@@ -1,11 +1,45 @@
 extends CharacterBody2D
 
+const SPEED = 200.0
+const JUMP_VELOCITY = -500.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animation = $AnimatedSprite2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	# Animations
+	if (velocity.x < 0):
+		animation.flip_h = false
+		animation.play("run")
+	elif (velocity.x > 0):
+		animation.flip_h = true
+		animation.play("run")
+	else:
+		animation.play("idle")
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		animation.play("jump")
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animation.play("jump")
+		velocity.y = JUMP_VELOCITY
+
+	# Handle duck.
+	if Input.is_action_pressed("duck") and is_on_floor():
+		animation.play("duck")
+		position.y += 1
+
+	var direction = Input.get_axis("left", "right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 20)
+
+	move_and_slide()
+	
